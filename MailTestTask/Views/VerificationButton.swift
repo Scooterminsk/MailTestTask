598bcb23d4967ddcc75dcf7db8df857799385fdd
Scooterminsk <kickscooterby@gmail.com>
2022-09-12 17:44:02 +0300

    Verification button was added

---
+++ MailTestTask/Views/VerificationButton.swift
@@ -0,0 +1,33 @@
+//
+//  VerificationButton.swift
+//  MailTestTask
+//
+//  Created by Zenya Kirilov on 12.09.22.
+//
+
+import Foundation
+import UIKit
+
+class VerificationButton: UIButton {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        backgroundColor = #colorLiteral(red: 0.9450980392, green: 0.9333333333, blue: 0.862745098, alpha: 1)
+        setTitle("Verification button", for: .normal)
+        let color = #colorLiteral(red: 0.2156862745, green: 0.2156862745, blue: 0.2156862745, alpha: 1)
+        setTitleColor(color, for: .normal)
+        layer.cornerRadius = 10
+        titleLabel?.font = UIFont(name: "Avenir Book", size: 17)
+        isEnabled = false
+        alpha = 0.5
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
