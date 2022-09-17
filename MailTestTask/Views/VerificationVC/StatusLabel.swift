235ea3321b44e3d3e9c122d25057b25bae5ac991
Scooterminsk <kickscooterby@gmail.com>
2022-09-17 17:58:18 +0300

    StatusLabel + VerificationButton settings

--- MailTestTask/Views/VerificationVC/StatusLabel.swift
+++ MailTestTask/Views/VerificationVC/StatusLabel.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class StatusLabel: UILabel {
     
+    public var isValid = false {
+        didSet {
+            if self.isValid {
+                setValidSetting()
+            } else {
+                setNotValidSetting()
+            }
+        }
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
@@ -26,5 +36,15 @@ class StatusLabel: UILabel {
         font = UIFont(name: "Apple SD Gothic Neo", size: 16)
         adjustsFontSizeToFitWidth = true
         translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func setNotValidSetting() {
+        text = "Mail is not valid. Example: name@domain.ru"
+        textColor = #colorLiteral(red: 0.5215686275, green: 0.1098039216, blue: 0.05098039216, alpha: 1)
+    }
+    
+    private func setValidSetting() {
+        text = "Mail is not valid"
+        textColor = #colorLiteral(red: 0.1960784314, green: 0.3411764706, blue: 0.1019607843, alpha: 1)
     }
 }
