f31e51c4ca357cf2391d3c1f591b86afb42107d6
Scooterminsk <kickscooterby@gmail.com>
2022-09-18 11:57:13 +0300

    API key was found and added

---
+++ MailTestTask/Network/MailApiKey.swift
@@ -0,0 +1,11 @@
+//
+//  MailApiKey.swift
+//  MailTestTask
+//
+//  Created by Zenya Kirilov on 18.09.22.
+//
+
+import Foundation
+import UIKit
+
+let apiKey = "39cfe533-260c-4095-af72-cd8442658615"
